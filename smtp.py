070f7939638481ef2c8e3ddebfbf9aa1296a9c87
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2023-05-04 22:31:24 -0400

    better attempt

--- smtp.py
+++ smtp.py
@@ -83,7 +83,8 @@ def send(socket, to, subject, body):
         rxtx(socket, base64.encode(email))
         rxtx(socket, base64.encode(password))
     except Exception as e:
-        print(__import__("sys").exc_info(), e)
+        import sys
+        print(sys.exc_info(), e)
         time.sleep(5)
         pass
     rxtx(socket, "MAIL FROM:{}".format(secrets["email"]))
